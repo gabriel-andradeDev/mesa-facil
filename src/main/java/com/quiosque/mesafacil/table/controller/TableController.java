7ab5b26bc54962287cdf3f45fd2ceee2e9119bd5
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-10 10:04:41 -0300

    Add updateTable method to TableController for table updates

--- src/main/java/com/quiosque/mesafacil/table/controller/TableController.java
+++ src/main/java/com/quiosque/mesafacil/table/controller/TableController.java
@@ -1,5 +1,6 @@
 package com.quiosque.mesafacil.table.controller;
 
+import com.quiosque.mesafacil.table.entity.TableEntity;
 import com.quiosque.mesafacil.table.service.TableService;
 import com.quiosque.mesafacil.table.dtos.CreateTableDTO;
 import com.quiosque.mesafacil.table.dtos.ResponseTableDTO;
@@ -27,5 +28,10 @@ public class TableController {
     @PostMapping
     public ResponseEntity<ResponseTableDTO> postTable(@RequestBody CreateTableDTO mesas, @CurrentSecurityContext(expression = "authentication.principal") UserEntity user) {
         return mesaService.createTable(mesas, user.getId());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseTableDTO> updateTable(@PathVariable Long id, @RequestBody CreateTableDTO mesas, @CurrentSecurityContext(expression = "authentication.principal") UserEntity user) {
+        return mesaService.updateTable(id, mesas, user.getId());
     }
 }

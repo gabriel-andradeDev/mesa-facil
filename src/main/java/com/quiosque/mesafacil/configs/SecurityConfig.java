9535921be4ccf0e1bcacfeaf96b69d336fb7bdba
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-12 14:30:40 -0300

    Add authentication requirement for PUT requests to /api/user/{id}

--- src/main/java/com/quiosque/mesafacil/configs/SecurityConfig.java
+++ src/main/java/com/quiosque/mesafacil/configs/SecurityConfig.java
@@ -7,6 +7,7 @@ import jakarta.servlet.DispatcherType;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -52,6 +54,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers("/api/auth").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/user/{id}").authenticated()
                         .requestMatchers("/api/user/**").hasRole(UserRole.ADMIN.name())
                         .anyRequest().authenticated()
                 )

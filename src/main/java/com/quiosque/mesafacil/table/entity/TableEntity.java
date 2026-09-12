b1465a70978d1e408becb8a1049dc5194e4cc304
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-12 14:51:55 -0300

    Add products relationship to TableEntity for product management

--- src/main/java/com/quiosque/mesafacil/table/entity/TableEntity.java
+++ src/main/java/com/quiosque/mesafacil/table/entity/TableEntity.java
@@ -1,5 +1,6 @@
 package com.quiosque.mesafacil.table.entity;
 
+import com.quiosque.mesafacil.product.entity.ProductEntity;
 import com.quiosque.mesafacil.table.enums.Status;
 import com.quiosque.mesafacil.user.entity.UserEntity;
 import jakarta.persistence.*;
@@ -8,6 +9,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "mesa")
@@ -38,6 +41,13 @@ public class TableEntity {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "admin_id", nullable = false)
     private UserEntity admin;
+
+    @OneToMany(
+            mappedBy = "mesa",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<ProductEntity> products = new ArrayList<>();
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)

af33d50a92ba46acfaf7a43c7f66289ef95d6372
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-12 14:30:45 -0300

    Add updateUser method to UserController for user updates

--- src/main/java/com/quiosque/mesafacil/user/controller/UserController.java
+++ src/main/java/com/quiosque/mesafacil/user/controller/UserController.java
@@ -55,4 +55,9 @@ public class UserController {
     public WaiterDTO getWaiterById(@PathVariable Long id) {
         return this.waiterService.getWaiterById(id);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ResponseUserDTO> updateUser(@PathVariable Long id, @RequestBody CreateUserDTO dto, @CurrentSecurityContext(expression = "authentication.principal") UserEntity user) {
+        return this.userService.updateUser(id, dto, user.getId());
+    }
 }
